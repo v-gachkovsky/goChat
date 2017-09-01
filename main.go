d68c2c6815e00f577c51d2df8c58f7ee66784976
Vladimir Gachkovsky <gachkovsky@waywireglobal.com>
2017-09-01 18:28:15 +0300

    one more try

--- main.go
+++ main.go
@@ -1,142 +1,65 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gorilla/websocket"
 	"github.com/gorilla/mux"
-	"fmt"
+	"net/http"
+	"log"
+	"github.com/gorilla/websocket"
 )
-
-var upgrader = websocket.Upgrader{}
-
-type Hub struct {
-	clients map[*Client]bool
-	broadcast     chan []byte
-	addClient     chan *Client
-	removeClient  chan *Client
-}
-
-var Hubs = make(map[string] Hub)
-
-func (hub *Hub) start() {
-	for {
-		select {
-		case conn := <-hub.addClient:
-			hub.clients[conn] = true
-		case conn := <-hub.removeClient:
-			if _, ok := hub.clients[conn]; ok {
-				delete(hub.clients, conn)
-				close(conn.send)
-			}
-		case message := <-hub.broadcast:
-			for conn := range hub.clients {
-				select {
-				case conn.send <- message:
-				default:
-					close(conn.send)
-					delete(hub.clients, conn)
-				}
-			}
-		}
-	}
-}
 
 type Client struct {
 	ws *websocket.Conn
 	send chan []byte
 }
 
-func (c *Client) write() {
-	defer func() {
-		c.ws.Close()
-	}()
+type Room struct {
+	broadcast chan []byte
+	join 			chan *Client
+	leave 		chan *Client
+	clients 	map[*Client]bool
 
-	for {
-		select {
-		case message, ok := <-c.send:
-			if !ok {
-				c.ws.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			c.ws.WriteMessage(websocket.TextMessage, message)
-		}
-	}
 }
 
-func (c *Client) read(hubId string) {
-	defer func() {
-		Hubs[hubId].removeClient <- c
-		c.ws.Close()
-	}()
+func (room *Room) listeningForClients() {
 
-	for {
-		_, message, err := c.ws.ReadMessage()
-		if err != nil {
-			Hubs[hubId].removeClient <- c
-			c.ws.Close()
-			break
-		}
-
-		Hubs[hubId].broadcast <- message
-	}
 }
 
-func wsPage(res http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	hubId := vars["hubId"]
-	fmt.Println(hubId)
+var Rooms = make(map[string]Room)
 
-	conn, err := upgrader.Upgrade(res, req, nil)
-
-	if err != nil {
-		http.NotFound(res, req)
-		return
+func main() {
+	Rooms["first"] = Room{
+		broadcast: make(chan []byte),
+		join: make(chan *Client),
+		leave: make(chan *Client),
+		clients: make(map[*Client]bool),
 	}
 
-	client := &Client{
-		ws:   conn,
-		send: make(chan []byte),
+	Rooms["second"] = Room{
+		broadcast: make(chan []byte),
+		join: make(chan *Client),
+		leave: make(chan *Client),
+		clients: make(map[*Client]bool),
 	}
 
-	Hubs[hubId].addClient <- client
-
-	go client.write()
-	go client.read(hubId)
-}
-
-func homePage(res http.ResponseWriter, req *http.Request){
-	http.ServeFile(res, req, "./public/index.html")
-}
-
-func main(){
-	Hubs["first"] = Hub{
-		broadcast:     make(chan []byte),
-		addClient:     make(chan *Client),
-		removeClient:  make(chan *Client),
-		clients:       make(map[*Client]bool),
+	for _, room := range Rooms {
+		log.Println(room)
 	}
 
-	Hubs["second"] = Hub{
-		broadcast:     make(chan []byte),
-		addClient:     make(chan *Client),
-		removeClient:  make(chan *Client),
-		clients:       make(map[*Client]bool),
-	}
+	router := mux.NewRouter().StrictSlash(true)
+	public := http.FileServer(http.Dir("./public"))
 
-	for _, hub := range Hubs {
-		go hub.start()
-	}
-
-	//go hub.start()
-	fs := http.FileServer(http.Dir("./public"))
-
-	router := mux.NewRouter()
-	router.HandleFunc("/ws/{hubId}", wsPage)
-
-	router.PathPrefix("/").Handler(fs)
+	router.HandleFunc("/ws/{id}", handleRoom)
+	router.PathPrefix("/").Handler(public)
 	http.Handle("/", router)
 
-	http.ListenAndServe(":8080", nil)
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+func handleRoom(w http.ResponseWriter, r *http.Request) {
+	log.Println(mux.Vars(r))
 }
